ca614a19487c20d98379e71d499a757ec97370af
Sergio Saavedra <sergio.saavedra@ug.uchile.cl>
2023-07-20 18:52:31 -0400

    update index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/index'
+  # Define index as root
+  root 'pages#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
